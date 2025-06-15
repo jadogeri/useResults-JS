4157923bb2b307bc5e65cf3ea11e34d5e62d7add
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-06-15 18:53:09 -0500

    Added README.MD

---
+++ index.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react"
+import { getParamsAsString } from "./src/getParamsAsString.js";
+
+export default ({baseURL, baseRoute, baseHeaders}) => {
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('')
+     const _baseURL = baseURL;
+     const _headers = baseHeaders? baseHeaders : {}
+
+    const searchApi = async ({route, params, headers}) => {        
+        try {
+            const _params = getParamsAsString(params)
+
+               // alert("inside else there is no key ")
+            const response =
+             await fetch(`${_baseURL}${route}${_params}`,
+                           {mode:'cors', 
+                            headers: headers? headers : _headers,
+                            
+                           });
+            const data = await response.json();
+
+            setResults(data)
+            setErrorMessage('')
+
+        } catch (e) {
+            console.log('something went wrong terminal')
+            console.log(e.message)
+
+
+            setErrorMessage('something went wrong\nerror message : ' + e.message)
+        }
+
+    }
+
+    useEffect(() => {
+        searchApi({baseURL: _baseURL,route: baseRoute, headers: _headers})
+    }, [])
+
+
+    return [searchApi, results, errorMessage];
+
+};
+
